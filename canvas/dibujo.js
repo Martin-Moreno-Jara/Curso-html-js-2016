ccde96c45cbbab9e77d0764f3b53017df7f7539e
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-01-26 12:46:37 -0500

    versión estable del lienzo con flechas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,6 +1,5 @@
-var d =document.getElementById("dibujito");
-var lienzo = d.getContext("2d")
-console.log(lienzo)
+var canva =document.getElementById("dibujito");
+var lienzo = canva.getContext("2d")
 
 var texto = document.getElementById("txt_lineas")
 var boton = document.getElementById("btn")
@@ -15,24 +14,26 @@ function makeStroke(initx,inity,finalx,finaly,color){
     lienzo.closePath();
 }
 
-makeStroke(1,1,0,300,"#AFA")
-makeStroke(1,1,300,0,"#AFA")
-makeStroke(299,299,0,299,"#AFA")
-makeStroke(299,299,299,0,"#AFA")
+
 // makeStroke(20,50,80,50,"black")
 // makeStroke(20,50,20,150,"red")
 // makeStroke(20,150,80,150,"blue")
 // makeStroke(80,150,80,50,"purple")
+function dibujar_bordes(){
+    makeStroke(1,1,0,300,"#AFA")
+    makeStroke(1,1,300,0,"#AFA")
+    makeStroke(299,299,0,299,"#AFA")
+    makeStroke(299,299,299,0,"#AFA")
+}
 
- function dibujar_torre(x,y){
+ function dibujar_torre(x,y,limit_lineas){
     var lineas = 0;
-    while(lineas<30){
-        //x=lineas*10
-        //y=10*(lineas+1)
-        makeStroke(0,y,x+10,300,"#AAFA");
+    console.log(canva.width)
+    while(lineas<limit_lineas){
+        makeStroke(0,y,x+(canva.width/limit_lineas),300,"#AAFA");
         lineas++;
-        x+=10;
-        y+=10;
+        x+=canva.width/limit_lineas;
+        y+=canva.height/limit_lineas;
     }   
  }
  function dibujar_torre_traversa(x,y){
@@ -64,12 +65,17 @@ makeStroke(299,299,299,0,"#AFA")
  }
 
 function dibujo_por_click(){
-    alert("hey")
+    lienzo.clearRect(0,0,canva.width,canva.height)
+    dibujar_bordes()
+    valor_texto = texto.value
+    var num_lineas = parseInt(valor_texto);
+    console.log(num_lineas);
+    dibujar_torre(0,0,num_lineas)
 
 }
 
-//  dibujar_torre(0,0)
-//  dibujar_torre_traversa(0,0)
-//  dibujar_torre_inversa(0,0)
-//  dibujar_torre_reinversa(0,0)
+//  dibujar_torre(0,0);
+//  dibujar_torre_traversa(0,0);
+//  dibujar_torre_inversa(0,0);
+//  dibujar_torre_reinversa(0,0);
 
---
+++ flechas/draw.js
@@ -0,0 +1,92 @@
+// DECLARATION OF VARIABLES ---------------------------------
+var calification = document.getElementById("grade"); 
+var button = document.getElementById("send_grade"); 
+button.addEventListener("click",button_works); 
+
+var largo_linea = document.getElementById("line_width")
+var grueso_linea = document.getElementById("line_height")
+var color_linea = document.getElementById("line_color")
+
+var canva = document.getElementById("draw_canvas"); 
+var lienzo = canva.getContext("2d"); 
+document.addEventListener("keyup",drawKeyboard);
+
+var keys = {
+    UP:38,
+    DOWN:40,
+    LEFT:37,
+    RIGHT:39,
+    W:87,
+    S:83,
+    A:65,
+    D:68
+};
+var posX=250
+var posY=250
+
+//------------------------------------------------------
+
+
+// FUNCTIONS -----------------------------------------
+function button_works(){
+    alert("I do indeed work correctly");
+    console.log("My baby's got a gun");
+}
+function drawKeyboard(evento){
+    var largo = parseInt(largo_linea.value) 
+    var grueso = parseInt(grueso_linea.value)
+    var color = color_linea.value
+    console.log(largo)
+    switch(evento.keyCode){
+        case keys.LEFT:
+            console.log("Left");
+            drawLine(posX,posY,keys.LEFT,color,largo,grueso);
+            posX-=largo;
+            console.log(largo)
+            break;
+        case keys.UP:
+            console.log("Up");
+            drawLine(posX,posY,keys.UP,color,largo,grueso);
+            posY-=largo;
+            console.log(posY)
+            break;
+        case keys.RIGHT:
+            console.log("Roight innit");
+            drawLine(posX,posY,keys.RIGHT,color,largo,grueso);
+            posX+=largo;
+            console.log(posX)
+            break;  
+        case keys.DOWN:
+            console.log("Down");
+            drawLine(posX,posY,keys.DOWN,color,largo,grueso);
+            posY+=largo
+            console.log(posY)
+
+            break;
+    }
+}
+
+function drawLine(posX,posY,direction,color,largo,grueso){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = grueso;
+    lienzo.moveTo(posX,posY);
+    console.log("largo:"+ largo+" y: "+posY)
+    if(direction==keys.LEFT){
+        posX-=largo;
+    }
+    if(direction==keys.RIGHT){
+        posX+=largo;
+    }
+    if(direction==keys.UP){
+        posY-=largo;
+    }
+    if(direction==keys.DOWN){
+        posY+=largo;
+    }
+    console.log("posición x: "+posX+", posición y: "+posY)
+    lienzo.lineTo(posX,posY)
+    lienzo.stroke();
+    lienzo.closePath(); 
+}
+//-----------------------------------------------
