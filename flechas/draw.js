a323d0388199d1a20ae188f234df96dd1ffe6fe2
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-01-27 10:34:55 -0500

    Versión funcional de dibujar lineas con el mouse

--- flechas/draw.js
+++ flechas/draw.js
@@ -1,7 +1,6 @@
 // DECLARATION OF VARIABLES ---------------------------------
 var calification = document.getElementById("grade"); 
 var button = document.getElementById("send_grade"); 
-button.addEventListener("click",button_works); 
 
 var largo_linea = document.getElementById("line_width")
 var grueso_linea = document.getElementById("line_height")
@@ -9,7 +8,15 @@ var color_linea = document.getElementById("line_color")
 
 var canva = document.getElementById("draw_canvas"); 
 var lienzo = canva.getContext("2d"); 
-document.addEventListener("keyup",drawKeyboard);
+var posX=250
+var posY=250
+
+var positionX;
+var positionY;
+// document.addEventListener("keyup",drawKeyboard);
+canva.addEventListener("mousedown",drawMouseDown)
+canva.addEventListener("mouseup",drawMouseUp)
+
 
 var keys = {
     UP:38,
@@ -21,58 +28,63 @@ var keys = {
     A:65,
     D:68
 };
-var posX=250
-var posY=250
-
 //------------------------------------------------------
 
 
 // FUNCTIONS -----------------------------------------
-function button_works(){
-    alert("I do indeed work correctly");
-    console.log("My baby's got a gun");
+
+function drawMouseDown(evento){
+    console.log(evento)
+    positionX = evento.offsetX;
+    positionY = evento.offsetY
 }
-function drawKeyboard(evento){
-    var largo = parseInt(largo_linea.value) 
-    var grueso = parseInt(grueso_linea.value)
-    var color = color_linea.value
-    console.log(largo)
-    if(largo !=0 && largo !=NaN && grueso !=0 && grueso !=NaN){
-    switch(evento.keyCode){
-        case keys.LEFT:
-            console.log("Left");
-            drawLine(posX,posY,keys.LEFT,color,largo,grueso);
-            posX-=largo;
-            console.log(largo)
-            break;
-        case keys.UP:
-            console.log("Up");
-            drawLine(posX,posY,keys.UP,color,largo,grueso);
-            posY-=largo;
-            console.log(posY)
-            break;
-        case keys.RIGHT:
-            console.log("Roight innit");
-            drawLine(posX,posY,keys.RIGHT,color,largo,grueso);
-            posX+=largo;
-            console.log(posX)
-            break;  
-        case keys.DOWN:
-            console.log("Down");
-            drawLine(posX,posY,keys.DOWN,color,largo,grueso);
-            posY+=largo
-            console.log(posY)
-            break;
-    }
-    }
+function drawMouseUp(evento){
+    console.log(evento)
+    drawLineMouse(positionX,positionY,evento.offsetX,evento.offsetY,color_linea.value,grueso_linea.value) 
 }
+
+
+// function drawKeyboard(evento){
+//     var largo = parseInt(largo_linea.value) 
+//     var grueso = parseInt(grueso_linea.value)
+//     var color = color_linea.value
+//     console.log(largo)
+//     if(largo !=0 && largo !=NaN && grueso !=0 && grueso !=NaN){
+//     switch(evento.keyCode){
+//         case keys.LEFT:
+//             console.log("Left");
+//             drawLine(posX,posY,keys.LEFT,color,largo,grueso);
+//             posX-=largo;
+//             console.log(largo)
+//             break;
+//         case keys.UP:
+//             console.log("Up");
+//             drawLine(posX,posY,keys.UP,color,largo,grueso);
+//             posY-=largo;
+//             console.log(posY)
+//             break;
+//         case keys.RIGHT:
+//             console.log("Roight innit");
+//             drawLine(posX,posY,keys.RIGHT,color,largo,grueso);
+//             posX+=largo;
+//             console.log(posX)
+//             break;  
+//         case keys.DOWN:
+//             console.log("Down");
+//             drawLine(posX,posY,keys.DOWN,color,largo,grueso);
+//             posY+=largo
+//             console.log(posY)
+//             break;
+//     }
+//     }
+// }
 
 function drawLine(posX,posY,direction,color,largo,grueso){
     lienzo.beginPath();
     lienzo.strokeStyle = color;
     lienzo.lineWidth = grueso;
     lienzo.moveTo(posX,posY);
-    console.log("largo:"+ largo+" y: "+posY)
+
     if(direction==keys.LEFT){
         posX-=largo;
     }
@@ -87,6 +99,16 @@ function drawLine(posX,posY,direction,color,largo,grueso){
     }
     console.log("posición x: "+posX+", posición y: "+posY)
     lienzo.lineTo(posX,posY)
+    lienzo.stroke();
+    lienzo.closePath(); 
+}
+function drawLineMouse(initX,initY,finalX,finalY,color,grueso){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = grueso;
+    lienzo.moveTo(initX,initY);
+    console.log("posición x: "+initX+", posición y: "+initY)
+    lienzo.lineTo(finalX,finalY)
     lienzo.stroke();
     lienzo.closePath(); 
 }
