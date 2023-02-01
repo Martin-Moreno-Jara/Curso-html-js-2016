98366acbdf09414313f79ef9b2a8f487588bef17
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-02-01 11:02:04 -0500

    se separan los script

---
+++ pakiman/instances.js
@@ -0,0 +1,10 @@
+var imagenes = [];
+imagenes["kauchin"] = "imgs/vaca.png"
+imagenes["pokacho"] = "imgs/pollo.png"
+imagenes["tocinauro"] = "imgs/cerdo.png"
+var kauchin = new Pakiman("kauchin","dirt",100,30);
+var pokacho = new Pakiman("pokacho","Lightning",80,50)
+var tocinauro = new Pakiman("tocinauro","Water",200,20)
+kauchin.show();
+pokacho.show();
+tocinauro.show();
--- pakiman/pakiman.js
+++ pakiman/pakiman.js
@@ -25,9 +25,3 @@ class Pakiman{
     }
 }
 
-var kauchin = new Pakiman("kauchin","dirt",100,30);
-var pokacho = new Pakiman("pokacho","Lightning",80,50)
-var tocinauro = new Pakiman("tocinauro","Water",200,20)
-kauchin.show();
-pokacho.show();
-tocinauro.show();
