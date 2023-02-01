5938872b2da77b101c320801c559536831f0d08f
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-02-01 11:19:25 -0500

    programa en menos lineas con for each

--- pakiman/instances.js
+++ pakiman/instances.js
@@ -2,9 +2,15 @@ var imagenes = [];
 imagenes["kauchin"] = "imgs/vaca.png"
 imagenes["pokacho"] = "imgs/pollo.png"
 imagenes["tocinauro"] = "imgs/cerdo.png"
-var kauchin = new Pakiman("kauchin","dirt",100,30);
-var pokacho = new Pakiman("pokacho","Lightning",80,50)
-var tocinauro = new Pakiman("tocinauro","Water",200,20)
-kauchin.show();
-pokacho.show();
-tocinauro.show();
+
+// kauchin.show();
+// pokacho.show();
+// tocinauro.show();
+var colection = [];
+colection.push(new Pakiman("kauchin","dirt",100,30));
+colection.push(new Pakiman("pokacho","Lightning",80,50));
+colection.push(new Pakiman("tocinauro","Water",200,20));
+
+for(var i of colection){
+    i.show();
+}
