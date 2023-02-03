60039ae2e29ded23d45be3d1b39a07b7e34e1e1b
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-02-03 10:34:13 -0500

    Versión completa del ATM

--- cashRegister/Billete.js
+++ cashRegister/Billete.js
@@ -2,6 +2,8 @@ class Billete{
     constructor(valor,cantidad){
         this.valor = valor;
         this.cantidad = cantidad
+        this.imagen = new Image();
+        this.imagen.src = images[valor]
     }
     getValor(){
         return this.valor;
@@ -9,4 +11,14 @@ class Billete{
     getCantidad(){
         return this.cantidad;
     }
+    imprimirImagen(){
+        return this.imagen;
+            //document.body.appendChild(this.imagen)
+    }
 }
+images = [];
+images[5] = "imgs/5_dollars.jpg"
+images[10] = "imgs/10_dollars.jpg"
+images[20] = "imgs/20_dollars.jpg"
+images[50] = "imgs/50_dollars.jpg"
+images[100] = "imgs/100_dollars.jpg"
--- cashRegister/cash.js
+++ cashRegister/cash.js
@@ -1,30 +1,25 @@
 var caja = [];
-var solicitado = 230;
-var vueltas = [];
+var dinero = document.getElementById("dinero");
+var button = document.getElementById("extraer");
+button.addEventListener("click",darVueltas)
+var result = document.getElementById("result")
+var bills = document.getElementById("bills")
+var etiquete = document.getElementById("damoney")
 
-var billete50 = new Billete(50,3);
-var billete20 = new Billete(20,2);
-var billete10 = new Billete(10,2);
-// var billete50 = {
-//     valor:50,
-//     cantidad:3
-// };
-// var billete20 = {
-//     valor:20,
-//     cantidad:3
-// };
-// var billete10 = {
-//     valor:10,
-//     cantidad:3
-// };
-caja.push(billete50);
-caja.push(billete20);
-caja.push(billete10);
-console.log(solicitado)
+caja.push(new Billete(100,5));
+caja.push(new Billete(50,10,));
+caja.push(new Billete(20,5));
+caja.push(new Billete(10,10));
+caja.push(new Billete(5,5))
+
+etiquete.innerHTML = "Nuestras reservas:<br/>billetes $100: "+caja[0].getCantidad()+"<br/>billetes $50: "+caja[1].getCantidad()+"<br/>billetes $20: "+caja[2].getCantidad()+"<br/>billetes $10: "+caja[3].getCantidad()+"<br/>billetes $5: "+caja[4].getCantidad()+"<hr/>";
+
 
 function darVueltas()
 {
-    for(i in caja){
+    var vueltas = [];
+    var solicitado = parseInt(dinero.value);
+    for(var i in caja){
         if(solicitado>0){
             var billValue = caja[i].getValor();
             var billQuantity = caja[i].getCantidad();
@@ -37,7 +32,30 @@ function darVueltas()
         } 
     }
     if(solicitado!=0){
-        console.log("No podemos dar esa cantidad")
+        result.innerHTML="No podemos dar esa cantidad<br/>";
     }
-    console.log(vueltas)
+    else{
+        console.log(vueltas)
+        for(var billete of vueltas){
+            if(billete.cantidad>0){
+                for(var i=0;i<vueltas.length;i++){
+                    if(billete.valor==caja[i].getValor()){
+                        caja[i].cantidad-=billete.cantidad;
+                        console.log(caja[i])
+                    }
+                }
+                etiquete.innerHTML = "Nuestras reservas:<br/>billetes $100: "+caja[0].getCantidad()+"<br/>billetes $50: "+caja[1].getCantidad()+"<br/>billetes $20: "+caja[2].getCantidad()+"<br/>billetes $10: "+caja[3].getCantidad()+"<br/>billetes $5: "+caja[4].getCantidad()+"<hr/>";
+
+                result.innerHTML+= billete.getCantidad()+ " billetes de $"+billete.getValor()+"<br/>";
+                for(var i=1;i<=billete.getCantidad();i++){
+                    var billImage = billete.imprimirImagen()
+                    bills.appendChild(billImage);
+                    bills.innerHTML+=" "
+
+                }
+                
+            }
+        }
+        result.innerHTML+="<hr/>"
+    }
 }
